c9c275268beb59e92f63c16f12923749660c5aad
nick-bennett <nbennett10@cnm.edu>
2026-06-03 10:50:24 -0600

    Initial commit w/ method stub.

---
+++ src/main/java/edu/cnm/deepdive/FizzBuzz.java
@@ -0,0 +1,25 @@
+package edu.cnm.deepdive;
+
+/**
+ * Implements the convert method for constructing the FizzBuzz representation of a numeric value.
+ * <p>Rules of FizzBuzz:</p>
+ * <ul>
+ *   <li>An integer which is not divisible by either 3 or 5 is converted to the string representation of that number.</li>
+ *   <li>An integer which is divisible by 3, but not by 5, is represented as "Fizz".</li>
+ *   <li>An integer which is divisible by 5, but not by 3, is represented as "Buzz".</li>
+ *   <li>An integer which is divisible by both 3 and 5 is represented as "FizzBuzz".</li>
+ * </ul>
+ */
+public class FizzBuzz {
+
+  /**
+   * Converts an int to its equivalent FizzBuzz string representation.
+   *
+   * @param value Input to be converted.
+   * @return FizzBuzz representatio of input.
+   */
+  public String convert(int value) {
+    throw new UnsupportedOperationException("Not yet implemented.");
+  }
+
+}
